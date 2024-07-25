37cf26a1da27d7bf1d691bfe66d1347fa2968be8
HyunJin, Lee <nagi5924@gmail.com>
2024-07-25 15:22:30 +0900

    [chore] webpack 빌드 최적화 적용

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 // 플러그인
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
@@ -14,7 +13,9 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true
     },
+    devtool: 'eval-source-map',
     resolve: {
         extensions: ['.ts', '.tsx', '.js'],
     },
@@ -28,6 +29,10 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 use: 'babel-loader',
+                options: {
+                    cacheDirectory: true,
+                },
+                include: path.resolve(__dirname, 'src'),
                 exclude: /node_modules/,
             },
             {
@@ -52,21 +57,34 @@ module.exports = {
         // html 파일을 생성해주는 플러그인
         new HtmlWebpackPlugin({
             template: './public/index.html', // 경로
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true,
+            },
         }),
-        // 빌드 시 기존 빌드 파일을 삭제해주는 플러그인
-        new CleanWebpackPlugin(),
         // CSS 를 별도의 파일로 추출해주는 플러그인(캐싱, 병렬 다운로드 가능)
         new MiniCssExtractPlugin({
-            filename: '[name].css', // 추출된 CSS 파일 이름 설정
-            chunkFilename: '[id].css',
+            filename: '[name].[contenthash].css', // 추출된 CSS 파일 이름 설정
+            chunkFilename: '[id].[contenthash].css',
         }),
         // 번들크기 시각화 플러그인 (빌드 후에 'http://127.0.0.1:8888' 혹은 dist > report.html 에서 확인 가능)
         new BundleAnalyzerPlugin(),
         // 빌드 시 커밋, 사용자, 날짜를 출력하는 커스텀 플러그인
-        new webpack.BannerPlugin(banner)
+        new webpack.BannerPlugin(banner),
+        // 빌드 시 전체가 아닌 변경된 모듈만 다시 빌드하는 플러그인
+        new webpack.HotModuleReplacementPlugin(),
     ],
     // JS 파일 압축 플러그인
     optimization: {
+        minimize: true,
         minimizer: [
             new TerserWebpackPlugin({
                 parallel: true,
@@ -77,5 +95,10 @@ module.exports = {
         static: path.join(__dirname, 'dist'),
         compress: true,
         port: 3000,
+        hot: true, // Enable HMR
+    },
+    // 캐시 설정
+    cache: {
+        type: 'filesystem', // Enable Webpack filesystem caching
     },
 };
