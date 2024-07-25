25d6064ae9833ad9eb0583094a9ff11e302a7de4
lhj5924 <lhj5924@users.noreply.github.com>
2024-07-25 08:05:41 +0000

    deploy: 95345f805b10cfa1d6f699bcad1f453f79dbdfa7

--- src/services/ApiService.tsx
+++
@@ -1,9 +0,0 @@
-import { Post } from '../models/Postmodel';
-
-export const fetchPost = async (postId: number): Promise<Post> => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
-    if (!response.ok) {
-        throw new Error('Failed to fetch post');
-    }
-    return response.json();
-};
