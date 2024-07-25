09c7816caa47dc90397ac904cc7e0083c030cdc1
HyunJin, Lee <nagi5924@gmail.com>
2024-07-25 15:45:42 +0900

    [chore] babel-loader, file-loader 설정 변경 - babel 설정에서 options 속성을 use 배열 밖으로 꺼냄 - file-loader 대신 asset/resource 사용(웹팩5)

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-// 플러그인
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
@@ -28,35 +27,42 @@ module.exports = {
             },
             {
                 test: /\.(js|jsx)$/,
-                use: 'babel-loader',
-                options: {
-                    cacheDirectory: true,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                    },
                 },
                 include: path.resolve(__dirname, 'src'),
                 exclude: /node_modules/,
             },
             {
-                test: /\.(scss|css)$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-                exclude: /node_modules/,
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(scss|sass)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
             },
             {
                 test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[path][name].[ext]',
-                        },
-                    },
-                ],
+                type: 'asset/resource', // Webpack 5에서 파일 로더 대체
+                generator: {
+                    filename: 'assets/[hash][ext][query]'
+                }
             },
         ],
     },
     plugins: [
-        // html 파일을 생성해주는 플러그인
         new HtmlWebpackPlugin({
-            template: './public/index.html', // 경로
+            template: './public/index.html',
             minify: {
                 removeComments: true,
                 collapseWhitespace: true,
@@ -70,19 +76,14 @@ module.exports = {
                 minifyURLs: true,
             },
         }),
-        // CSS 를 별도의 파일로 추출해주는 플러그인(캐싱, 병렬 다운로드 가능)
         new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css', // 추출된 CSS 파일 이름 설정
+            filename: '[name].[contenthash].css',
             chunkFilename: '[id].[contenthash].css',
         }),
-        // 번들크기 시각화 플러그인 (빌드 후에 'http://127.0.0.1:8888' 혹은 dist > report.html 에서 확인 가능)
         new BundleAnalyzerPlugin(),
-        // 빌드 시 커밋, 사용자, 날짜를 출력하는 커스텀 플러그인
         new webpack.BannerPlugin(banner),
-        // 빌드 시 전체가 아닌 변경된 모듈만 다시 빌드하는 플러그인
         new webpack.HotModuleReplacementPlugin(),
     ],
-    // JS 파일 압축 플러그인
     optimization: {
         minimize: true,
         minimizer: [
@@ -95,10 +96,9 @@ module.exports = {
         static: path.join(__dirname, 'dist'),
         compress: true,
         port: 3000,
-        hot: true, // Enable HMR
+        hot: true,
     },
-    // 캐시 설정
     cache: {
-        type: 'filesystem', // Enable Webpack filesystem caching
+        type: 'filesystem',
     },
 };
